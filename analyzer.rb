842fa48d1842511192e30cf476f101848fc3975b
Niranjan7078 <neerureddy111@gmail.com>
2022-03-06 22:39:23 +0530

    first commit

---
+++ analyzer.rb
@@ -0,0 +1,8 @@
+puts "enter your first name"
+first_name =  gets.chomp
+puts "enter your last name"
+last_name  = gets.chomp
+puts "your full name is #{first_name} #{last_name}"
+puts "your full name reversed is #{first_name.reverse} #{last_name.reverse}"
+puts "length is #{first_name.length}"
+ 
---
+++ test.rb
@@ -0,0 +1,4 @@
+z = ["wqqw" , "eqdd" , "ewe"]
+for i in z
+    puts i  + " "
+end
